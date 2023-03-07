3728461feb07652889af2ff741db7ec654d2ee5e
Wanying Zhang <mimi@Wanyings-Air.fios-router.home>
2023-03-07 17:40:11 -0500

    server and extension separated. intial js and python done.

---
+++ extension/js/contentScript.js
@@ -0,0 +1,25 @@
+console.log("HELLO")
+
+document.getElementById("myButton").addEventListener("click", myFunction);
+
+function myFunction(){
+    let images = document.getElementsByTagName('img');
+    console.log(images)
+    console.log ("hello")
+    // for (let i = 0; i < images.length; i++) {
+    //     chrome.runtime.sendMessage({ msg: 'image', index: i }, function ({ data, index }) {
+    //         images[index].src = data.link;
+    //     });
+    // }
+    
+    const s = JSON.stringify(images); // Stringify converts a JavaScript object or value to a JSON string (not sure whether this is necessary for us?)
+    // console.log(s); // Prints the variables to console window, which are in the JSON format
+    
+    window.alert(s)
+    
+    $.ajax({
+        url:"http://127.0.0.1:5000/test",
+        type:"POST",
+        contentType: "application/json",
+        data: JSON.stringify(s)});
+}
---
+++ server/jstopy.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+import json
+from flask import Flask, render_template
+#Set up Flask</strong>:
+app = Flask(__name__)
+
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+@app.route('/test', methods=['POST']) # posts the function below
+def test(): 
+    # output = request.get_json() # "Parses the incoming JSON request data and returns it"
+    # print(output) # This is the output that was stored in the JSON within the browser
+    # print(type(output))
+    # result = json.loads(output) # This converts the json output to a python dictionary
+    # print(result) # Printing the new dictionary
+    # print(type(result)) # This shows the json converted as a python dictionary
+    return "123"
+
+if __name__ == '__main__':
+    app.run(port=5000,debug=True)

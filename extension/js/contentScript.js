a61e6bb0fc88070dab7759de9e1eb618882edfa9
cassidyxu <68167806+cassidyxu@users.noreply.github.com>
2023-03-10 20:20:59 -0500

    Update contentScript.js

--- extension/js/contentScript.js
+++ extension/js/contentScript.js
@@ -5,12 +5,11 @@ document.getElementById("myButton").addEventListener("click", myFunction);
 function myFunction(){
     let images = document.getElementsByTagName('img');
     console.log(images)
-    console.log ("hello")
-    // for (let i = 0; i < images.length; i++) {
-    //     chrome.runtime.sendMessage({ msg: 'image', index: i }, function ({ data, index }) {
-    //         images[index].src = data.link;
-    //     });
-    // }
+    
+    const links = new Array();
+    for (let i = 0; i < images.length; i++){
+        links.push(images[i].currentSrc);
+    }
     
     const s = JSON.stringify(images); // Stringify converts a JavaScript object or value to a JSON string (not sure whether this is necessary for us?)
     // console.log(s); // Prints the variables to console window, which are in the JSON format

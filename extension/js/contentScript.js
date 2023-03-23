8bdb89c57bbc18311f7e0b636bb349e0e2c99139
Wanying Zhang <mimi@Wanyings-Air.fios-router.home>
2023-03-22 22:20:40 -0400

    contentScript.js actually fixed this time.

--- extension/js/contentScript.js
+++ extension/js/contentScript.js
@@ -1,40 +1,45 @@
 document.getElementById("myButton").addEventListener("click", myFunction);
 
-var s = JSON.stringify("abc") //not sure how to get rid of this without messing everything up. 
-// basically this just means that the first input to test should be ignored ^
 function myFunction() {
-    chrome.tabs.query({ active: true, currentWindow: true }).then(function (tabs) { 
-      var activeTab = tabs[0]; 
-      var activeTabId = activeTab.id;
-      return chrome.scripting.executeScript({
-        target: { tabId: activeTabId },
-        injectImmediately: true,  // uncomment this to make it execute straight away, other wise it will wait for document_idle
-        func: DOMtoString,
-        args: ['img']  // you can use this to target what element to get the html for
-      });
-    }).then(function (results) { // after this stuff runs, return the result to message
-      s = JSON.stringify(results[0].result);
-      console.log(s)
-    }).catch(function (error) { // if something goes wrong
-      s = JSON.stringify('There was an error injecting script : \n' + error.message);
+  chrome.tabs.query({ active: true, currentWindow: true }).then(function (tabs) {
+    var activeTab = tabs[0];
+    var activeTabId = activeTab.id;
+    return chrome.scripting.executeScript({
+      target: { tabId: activeTabId },
+      injectImmediately: true,  // uncomment this to make it execute straight away, other wise it will wait for document_idle
+      func: DOMtoString,
+      args: ['img']  // you can use this to target what element to get the html for
     });
+  }).then(function (results) { // after this stuff runs, return the result to message
+    s = JSON.stringify(results[0].result);
+    console.log(s)
     $.ajax({ // sends it to test
-        url:"http://127.0.0.1:5000/test",
-        type:"POST",
-        contentType: "application/json",
-        data: JSON.stringify(s)
+      url: "http://127.0.0.1:5000/test",
+      type: "POST",
+      contentType: "application/json",
+      data: JSON.stringify(s)
     });
+  }).catch(function (error) { // if something goes wrong
+    s = JSON.stringify('There was an error injecting script : \n' + error.message);
+    $.ajax({ // sends it to test
+      url: "http://127.0.0.1:5000/test",
+      type: "POST",
+      contentType: "application/json",
+      data: JSON.stringify(s)
+    });
+  })
+  
+}
+
+
+function DOMtoString(selector) { // selector is img bc we're looking for images
+  if (selector) {
+    selector = document.querySelector(selector);
+    if (!selector) return "ERROR: querySelector failed to find node"
+  } else {
+    selector = document.documentElement;
   }
-
-
-  function DOMtoString(selector) { // selector is img bc we're looking for images
-    if (selector) {
-        selector = document.querySelector(selector);
-        if (!selector) return "ERROR: querySelector failed to find node"
-    } else {
-        selector = document.documentElement;
-    }
-    return selector.src; //right now this is getting the link. if you want everything in the image tag, use "selector.outerHTML"
+  return selector.src; //right now this is getting the link. if you want everything in the image tag, use "selector.outerHTML"
 }
 
 /*
